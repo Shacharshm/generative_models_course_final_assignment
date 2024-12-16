2dad56cc4596b5e547a259c7f9b630c8b5022cc5
shacharoctup <shachar.shmueli@octup.com>
2024-12-16 19:26:31 +0200

    Added safety guidance

--- src/llm_guidance.py
+++ src/llm_guidance.py
@@ -3,7 +3,7 @@ from transformers import LogitsProcessorList, StoppingCriteriaList
 from typing import Optional, Union
 import torch
 import logging
-import os
+import torch.nn.functional as F
 
 logging.basicConfig(level=logging.INFO)
 
@@ -46,37 +46,6 @@ class LLMPipeline:
         for name, module in kwargs.items():
             setattr(self, name, module)
 
-    def enable_sequential_cpu_offload(self):
-        """
-        Offloads the model to CPU and loads it to GPU only during forward pass to save memory.
-        Requires `accelerate`.
-        """
-        if not hasattr(self.model, 'cpu_offload'):
-            raise ValueError("Sequential CPU offloading is not supported for this model.")
-        self.model.cpu_offload()
-
-    def disable_sequential_cpu_offload(self):
-        """
-        Disables sequential CPU offload.
-        """
-        if hasattr(self.model, 'cpu_offload'):
-            self.model.cpu_offload(False)
-
-    def enable_attention_slicing(self, slice_size: Optional[Union[str, int]] = "auto"):
-        """
-        Enable sliced attention computation to save memory.
-
-        Args:
-            slice_size (str or int): Size of the slices to compute attention. Defaults to "auto".
-        """
-        if hasattr(self.model, 'set_attention_slice'):
-            self.model.set_attention_slice(slice_size)
-
-    def disable_attention_slicing(self):
-        """Disables attention slicing."""
-        if hasattr(self.model, 'set_attention_slice'):
-            self.model.set_attention_slice(None)
-
     def get_embeddings(self, input_ids: torch.Tensor) -> torch.Tensor:
         if hasattr(self.model, 'transformer'):
             # GPT-like models (e.g., GPT-2, GPT-J)
@@ -96,6 +65,62 @@ class LLMPipeline:
         else:
             raise ValueError(f"Model {self.model_name} does not have accessible embeddings.")
 
+    def apply_safety_guidance(self, 
+                              embeddings: torch.Tensor, 
+                              safety_concept_embeddings: torch.Tensor, 
+                              guidance_scale: float,
+                              safety_momentum: Optional[torch.Tensor] = None, 
+                              guidance_threshold: float = 0.0,
+                              guidance_momentum_scale: float = 0.0,
+                              guidance_mom_beta: float = 0.0,
+                              ) -> torch.Tensor:
+        """
+        Apply safety guidance to the embeddings.
+
+        Args:
+            embeddings (torch.Tensor):
+                Embeddings to apply safety guidance to.
+            safety_concept_embeddings (torch.Tensor):
+                Embeddings of safety concepts.
+            guidance_scale (float):
+                Scale factor for the guidance.
+            safety_momentum (torch.Tensor):
+                Momentum from the previous step.
+            guidance_threshold (float):
+                Threshold for the guidance.
+            guidance_momentum_scale (float):
+                Scale factor for the guidance momentum.
+            guidance_mom_beta (float):
+                Beta factor for the guidance momentum.
+
+        Returns:
+            torch.Tensor: Guided embeddings.
+        """
+        if safety_momentum is None:
+            safety_momentum = torch.zeros_like(embeddings)
+
+        # Equation 6
+        scale = torch.clamp(
+            torch.abs((embeddings - safety_concept_embeddings)) * guidance_scale, max=1.)
+
+        # Equation 6
+        safety_concept_scale = torch.where(
+            (embeddings - safety_concept_embeddings) >= guidance_threshold,
+            torch.zeros_like(scale), scale)
+
+        # Equation 4
+        guidance_safety_emb = torch.mul(embeddings, safety_concept_scale)
+
+        # Equation 7
+        noise_guidance_safety = guidance_safety_emb + guidance_momentum_scale * safety_momentum
+
+        # Equation 8
+        safety_momentum = guidance_mom_beta * safety_momentum + (1 - guidance_mom_beta) * noise_guidance_safety
+
+        return safety_momentum
+        
+
+
     @torch.no_grad()
     def __call__(
         self,
@@ -108,6 +133,7 @@ class LLMPipeline:
         stopping_criteria: Optional[StoppingCriteriaList] = None,
         logits_processor: Optional[LogitsProcessorList] = None,
         guidance_scale: float = 0.0,
+        verbose: bool = False
     ) -> str:
         """
         Perform text generation given an input prompt.
@@ -139,25 +165,32 @@ class LLMPipeline:
             enable_safety_guidance = False
             self.logger.warning('You have disabled safety guidance.')
 
-        # Tokenize the input prompt
-        input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
-        # input_ids = input_ids.to(self.model.device)
-        input_embeddings = self.get_embeddings(input_ids)
-
         if enable_safety_guidance:
             safety_concept_ids = self.tokenizer(self.safety_concepts, return_tensors="pt").input_ids
-
-        current_input = input_embeddings
-        generated_tokens = input_ids
+            safety_concept_embeddings = self.get_embeddings(safety_concept_ids)
+            safety_concept_embeddings = torch.mean(safety_concept_embeddings, dim=1, keepdim=True)
+            safety_concept_embeddings = safety_concept_embeddings.to(self.model.device)
         
+        # Tokenize the input prompt
+        input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
+        input_embeddings = self.get_embeddings(input_ids)
+
+
+        current_input = input_embeddings.to(self.model.device)
+        generated_tokens = input_ids.to(self.model.device)
+
         for i, _ in enumerate(range(max_length - 1)):
+
+            if verbose:
+                self.logger.info(f"Step {i+1}/{max_length}")
+
             # Forward pass with custom embeddings
             outputs = model(inputs_embeds=current_input)
             logits = outputs.logits
 
-            # Get the next token from the logits (using argmax for simplicity)
-            next_token = logits[:, -1, :] #.argmax(dim=-1).unsqueeze(-1)
-            next_token_probs = torch.nn.functional.softmax(next_token, dim=-1)
+            # Get the next token from the logits
+            next_token = logits[:, -1, :]
+            next_token_probs = F.softmax(next_token, dim=-1)
             next_token = torch.multinomial(next_token_probs, num_samples=1)
 
             # Append the next token to the generated sequence
@@ -165,7 +198,11 @@ class LLMPipeline:
 
             # Get the embedding for the new token and use it as input for the next step
             new_embedding = self.get_embeddings(next_token)
+            if enable_safety_guidance:
+                new_embedding = self.apply_safety_guidance(new_embedding, safety_concept_embeddings, guidance_scale)
+                
             current_input = torch.cat((current_input, new_embedding), dim=1)
+                
 
         # Decode and return the generated text
         generated_text = self.tokenizer.decode(generated_tokens[0], skip_special_tokens=True)
@@ -176,13 +213,14 @@ class LLMPipeline:
 if __name__ == "__main__":
     from transformers import AutoModelForCausalLM, AutoTokenizer
     from huggingface_hub import login
+    import os
 
     login(os.environ.get("HF_READ_TOKEN"))
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load a model and tokenizer
-    model_name = "google/gemma-7b"
+    model_name = "gpt2"
     if device.type == "cpu":
         model = AutoModelForCausalLM.from_pretrained(model_name)
         tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -203,5 +241,5 @@ if __name__ == "__main__":
 
     # Run text generation
     prompt = "please tell me a sentence about the usa"
-    output = llm_pipeline(prompt, max_length=100)
+    output = llm_pipeline(prompt, max_length=100, verbose=True, guidance_scale=1.1)
     print(output)
