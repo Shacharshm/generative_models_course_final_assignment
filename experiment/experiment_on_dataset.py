376b8ccd0c50e6d6fb4b0a9a99d67e5e5d6a2baa
SamVerzhik <samverzhik@gmail.com>
2025-01-05 23:47:32 +0200

    Dataset update

--- experiment/experiment_on_dataset.py
+++ experiment/experiment_on_dataset.py
@@ -11,7 +11,7 @@ results_dir = os.path.join(base_dir, "results", timestamp)
 os.makedirs(results_dir)
 
 # get all csv path
-dataset_dir=os.path.join(base_dir,"dataset")
+dataset_dir = os.path.join(base_dir,"dataset")
 all_csv_files = [file
                  for path, subdir, files in os.walk(dataset_dir)
                  for file in glob(os.path.join(path, "*.csv"))]
@@ -21,7 +21,6 @@ for category in all_csv_files:
     # print(category)
     csv_file_to_run=category
 
-    # results_file_name= os.path.splitext(os.path.basename(csv_file_to_run))[0]
     results_file_name= os.path.basename(csv_file_to_run)
     df = pd.read_csv(csv_file_to_run, header=None)
     df.rename(columns={0: 'Prompt'}, inplace=True)
@@ -31,10 +30,10 @@ for category in all_csv_files:
     for index, row in df.iterrows():
         prompt=row['Prompt']
 
-        ## Pipeline here!
+        ###### Pipeline here! #####
 
         df.loc[index, 'Output'] = "blabla" #output
-        # print(df)
+
 
     # save results
     df.to_csv(os.path.join(results_dir,results_file_name))
